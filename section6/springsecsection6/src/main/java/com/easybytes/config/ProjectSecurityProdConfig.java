2ebc7cc317f18d6e963342c4ea04834f1f785f4c
Aditya Singh <aditya.singh04@sap.com>
2025-12-05 21:08:29 +0530

    Added new section - section 6 for the spring security application

---
+++ section6/springsecsection6/src/main/java/com/easybytes/config/ProjectSecurityProdConfig.java
@@ -0,0 +1,52 @@
+package com.easybytes.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@Profile("prod")
+public class ProjectSecurityProdConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
+//        http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        (requests) -> requests
+                                .requestMatchers("/myAccount", "/myBalance", "/myCards", "/myLoans").authenticated()
+                                .requestMatchers("/contact", "/notices", "/error", "/register").permitAll()
+                );
+
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+    /*@Bean
+    public UserDetailsService userDetailsService(DataSource ds) {
+        return new JdbcUserDetailsManager(ds);
+    }*/
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
+    }
+
+}
